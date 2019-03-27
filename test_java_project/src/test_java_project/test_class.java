26196de0de1b03ba5804c3334c430b694b9e8b31
SJM <shoncmjm@naver.com>
2019-03-27 11:36:11 +0900

    hotfix branch change a codew

--- test_java_project/src/test_java_project/test_class.java
+++ test_java_project/src/test_java_project/test_class.java
@@ -3,5 +3,6 @@ package test_java_project;
 public class test_class {
 	public static void main(String[] args) {
 		System.out.println("hello world~! hotfix!");
+		System.out.println("i`m the hotfix!");
 	}
 }
