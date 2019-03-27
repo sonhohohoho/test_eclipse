68ed8a2ad3be30feb53fbd6f195ee73590a9b4ac
SJM <shoncmjm@naver.com>
2019-03-27 11:32:36 +0900

    fd

--- test_java_project/src/test_java_project/test_class.java
+++ test_java_project/src/test_java_project/test_class.java
@@ -2,7 +2,7 @@ package test_java_project;
 
 public class test_class {
 	public static void main(String[] args) {
-		System.out.println("hello world~! hotfix!");
+		System.out.println("hello world~! hotfix!");h
 		System.out.println("i`m the master!");
 	}
 }
